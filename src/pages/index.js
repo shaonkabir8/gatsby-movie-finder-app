d49c42fafd6127657b021309a84962de542be045
Shaonkabir <shaonkabir98@gmail.com>
2020-02-27 10:49:44 +0600

    Refactor: add Theme Options using styled-components and update Global Styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,27 @@
 import React from "react"
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from '../styles/GlobalStyles'
 
-export default () => <div>Hello world!</div>
+// Define Theme Options
+const theme = {
+    color: {
+      bg: '#0c0f26',
+      light: '#171934',
+      yellow: '#faca31',
+      link: '#7678fb'
+    },
+    transition: 'all .3s linear'
+  }
+
+
+const Index = () => {
+    return (
+        <ThemeProvider theme={theme}>
+            <GlobalStyles/>
+            Hello Bangladesh
+        </ThemeProvider>
+    )
+}
+
+
+export default Index;
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,68 @@
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyles = createGlobalStyle`
+ @import url('https://fonts.googleapis.com/css?family=Lora:400,700,700i|Merriweather:400,400i&display=swap');
+
+  * {
+  	margin: 0;
+  	padding: 0;
+  	box-sizing: border-box;
+  }
+  body {
+    font-family: 'Merriweather',serif;
+    font-size: 16px;
+    line-height: 1.7;
+    background: ${props => props.theme.color.bg};
+    color: #fff;
+  }
+  h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+        margin: 0 0 15px;
+        font-weight: 700;
+        font-family: 'Lora', serif;
+    }
+
+  a:hover {text-decoration:none}
+  ol,ul {
+  	margin: 0;
+  	padding: 0;
+  	list-style:none;
+  }
+  .boxed-btn {
+    display: inline-block;
+    position: relative;
+    z-index: 1;
+    border: 2px solid ${props => props.theme.color.light};
+    color: hsla(0,0%,100%,.85);
+    background: transparent;
+    padding: 5px 25px;
+    margin-right: 15px;
+    border-radius: 4px;
+  }
+  .boxed-btn:hover {
+    color: #fff;
+    text-decoration:none;
+  }
+  .boxed-btn:after {
+    position: absolute;
+    left: 0;
+    top:0;
+    width: 0;
+    height: 100%;
+    content: '';
+    background: ${props => props.theme.color.yellow};
+    z-index: -1;
+  }
+  .boxed-btn:hover:after {
+    width: 100%;
+    transition: ${props => props.theme.transition};
+  }
+  
+
+`
+
+export default GlobalStyles
