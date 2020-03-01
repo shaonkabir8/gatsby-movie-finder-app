49f889bcc3091887b46cd42213031b3c1b4ff90d
Shaonkabir <shaonkabir98@gmail.com>
2020-03-01 18:06:29 +0600

    Refactor(Header): remove Bootstrap Markup from Header Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,26 @@
 import React from "react"
 import { Link } from "gatsby"
 import HeaderStyle from "../styles/HeaderStyle"
-import Head from './Head'
+import Head from "./Head"
 
 const Header = () => (
   <HeaderStyle>
-    <Head/>
+    <Head />
     <header className="header">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <div className="logo">
-              <Link to="/">Movie Finder</Link>
-            </div>
-          </div>
-          <div className="col-md-6">
-            <div className="search-bar">
-              <form action="" className="form search-form">
-                <input
-                  type="text"
-                  placeholder="Search Movie"
-                  className="form-control"
-                />
-              </form>
-            </div>
-          </div>
-          <div className="col-md-2">
-            <div className="dropdown_item text-center">
-              <p>Popular</p>
-            </div>
-          </div>
-        </div>
+      <div className="logo">
+        <Link to="/">Movie Finder</Link>
+      </div>
+      <div className="search-bar">
+        <form action="" className=" search-form">
+          <input
+            type="text"
+            placeholder="Search Movie"
+            className="fo"
+          />
+        </form>
+      </div>
+      <div className="dropdown_item text-center">
+        <p>Popular</p>
       </div>
     </header>
   </HeaderStyle>
--- src/styles/HeaderStyle.js
+++ src/styles/HeaderStyle.js
@@ -1,36 +1,52 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 const HeaderStyle = styled.div`
   .header {
-		padding: 20px 0;
+    padding: 20px 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: baseline;
 
+    .logo {
+      padding-left: 46px;
+    }
 
-		.logo a {
-			color: ${props => props.theme.color.yellow};
-			padding: 4px 8px;
-			border: 1px solid;
-			border-radius: 3px;
-			display: inline-block;
-			font-size: 20px;
-			transition: ${props => props.theme.transition};
+    .search-bar {
+      width: 40%;
+    }
 
-			&:hover {
-				color: ${props => props.theme.color.link};
-			}
-	}
+    .dropdown_item p {
+      color: #fff;
+      font-weight: bold;
+    }
 
+    .logo a {
+      color: ${props => props.theme.color.yellow};
+      padding: 4px 8px;
+      border: 1px solid;
+      border-radius: 3px;
+      display: inline-block;
+      font-size: 20px;
+      transition: ${props => props.theme.transition};
 
-	input[type='text'] {
-		width: 100%;
-		background: ${props => props.theme.color.light};
-		color: ${props => props.theme.color.link};
-		border: 1px solid transparent;
+      &:hover {
+        color: ${props => props.theme.color.link};
+      }
+    }
 
-		&:focus {
-			box-shadow: 0 0 5px ${props => props.theme.color.link};
-		}
-	}
-}
-`;
+    input[type="text"] {
+      width: 100%;
+      background: ${props => props.theme.color.light};
+      color: ${props => props.theme.color.link};
+      border: 1px solid transparent;
+      padding: 9px 20px;
+      border-radius: 50px;
 
-export default HeaderStyle;
+      &:focus {
+        outline-color: transparent;
+      }
+    }
+  }
+`
+
+export default HeaderStyle
