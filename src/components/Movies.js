8de250b258e18897f3f214d0dd3d1f9b7bf88578
Shaonkabir <shaonkabir98@gmail.com>
2020-03-01 18:23:42 +0600

    Refactor: Fetched data and Methods are moved to Root Component

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react"
 import styled from 'styled-components';
 import Movie from "./Movie"
-import { showGenres } from '../utils/genres'
 
 const MovieStyle = styled.div`
 	display: flex;
@@ -13,27 +12,8 @@ const MovieStyle = styled.div`
 `;
 
 
-const Movies = () => {
-  // our API key from MovieDB
-  const apiKey = "8beeb3840481b08aae693a269c46433e"
-
-  // define our state
-  const [movies, setMovies] = useState([])
-
-  useEffect(() => {
-    fetch(`https://api.themoviedb.org/4/list/1?page=1&api_key=${apiKey}`)
-      .then(res => res.json())
-      .then(json => setMovies(json.results))
-      .catch(err => console.log(err))
-  }, [])
-
-  // HandleGenres
-  const handleGenres = movie => {
-    return showGenres().map(m => (m.id === movie.genre_ids[0] ? m.name : null));
-  };
-
-
-
+const Movies = ({ movies, handleGenres }) => {
+  
   return (
     <MovieStyle>
       {movies.map(movie => {
--- src/components/Root.js
+++ src/components/Root.js
@@ -1,11 +1,12 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { ThemeProvider } from "styled-components"
 import GlobalStyles from "../styles/GlobalStyles"
-import { Container, FlexContainer } from '../styles/LayoutStyles';
+import { Container, FlexContainer } from "../styles/LayoutStyles"
 import Header from "./Header"
 import Footer from "./Footer"
-import SideBar from "./SideBar";
-import Movies from './Movies';
+import SideBar from "./SideBar"
+import Movies from "./Movies"
+import { showGenres } from '../utils/genres'
 
 // Define Theme Options
 const theme = {
@@ -19,6 +20,24 @@ const theme = {
 }
 
 const Root = () => {
+  // our API key from MovieDB
+  const apiKey = "8beeb3840481b08aae693a269c46433e"
+
+  // define our state
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/4/list/1?page=1&api_key=${apiKey}`)
+      .then(res => res.json())
+      .then(json => setMovies(json.results))
+      .catch(err => console.log(err))
+  }, [])
+
+  // HandleGenres
+  const handleGenres = movie => {
+    return showGenres().map(m => (m.id === movie.genre_ids[0] ? m.name : null))
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
@@ -26,7 +45,7 @@ const Root = () => {
       <Container>
         <FlexContainer>
           <SideBar />
-          <Movies />
+          <Movies movies={movies} handleGenres={handleGenres} />
         </FlexContainer>
       </Container>
       <Footer />
