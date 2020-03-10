b5e0478b3d1957da75cd61927114d0e08f05f786
Shaonkabir <shaonkabir98@gmail.com>
2020-03-10 23:27:43 +0600

    Fix(config): Restore deleted config file

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+    siteMetadata: {
+      title: 'Movie Finder',
+      author: 'Shaon Kabir'
+    },
+    plugins: [
+      `gatsby-plugin-styled-components`,
+      `gatsby-transformer-sharp`,
+      `gatsby-plugin-sharp`,
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          path: `${__dirname}/src/images/`,
+        },
+      },
+    ],
+  }
