1bea4c45b7fc27081b9347455e7880d2b8d8802d
Shaonkabir <shaonkabir98@gmail.com>
2020-03-11 03:33:06 +0600

    refactor(Movies): change markup for sass. remove styled-components

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,38 +1,33 @@
 import React from "react"
 import { Link } from 'react-router-dom'
-import {
-  MovieStyle,
-  MovieTitle,
-  Infocontainer,
-  MovieRatings
-} from "../styles/MovieStyles"
 
 
 const Movie = ({ movie , showGenres}) => {
   const { poster_path, original_title, vote_average, id, release_date } = movie
 
   return (
-    <MovieStyle>
-      <Link to={`/movie/${id}`} className="movie">
+    
+    <div className="movies">
+      <Link to={`/movie/${id}`} className="movies__link">
         <img
-          className="movie__img"
+          className="movies__img"
           src={`https://image.tmdb.org/t/p/w300${poster_path}`}
           alt={original_title}
         />
-        <MovieTitle>
+        <p className="movies__title">
           <strong>{original_title}</strong>
-        </MovieTitle>
-        <Infocontainer>
+        </p>
+        <div className="movies__info">
           <p>
             <strong>{showGenres(movie)}</strong>
           </p>
           <p>
             <strong>{release_date}</strong>
           </p>
-        </Infocontainer>
-        <MovieRatings><i className="fas fa-star"></i>{vote_average}</MovieRatings>
+        </div>
+        <p className="movies__ratings"><i className="fas fa-star"></i>{vote_average}</p>
       </Link>
-    </MovieStyle>
+    </div>
   )
 }
 
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,25 +1,15 @@
 import React from "react"
-import styled from 'styled-components';
 import Movie from "./Movie"
-
-const MovieStyle = styled.div`
-	display: flex;
-	flex-wrap: wrap;
-	width: 100%;
-	justify-content: space-between;
-	padding: 3rem;
-	background: ${props => props.theme.color.light};
-`;
 
 
 const Movies = ({ movies, handleGenres }) => {
   
   return (
-    <MovieStyle>
+    <div className="movies-container">
       {movies.map(movie => {
 		  return <Movie movie={movie} showGenres={handleGenres}/>
 	  })}
-    </MovieStyle>
+    </div>
   )
 }
 
