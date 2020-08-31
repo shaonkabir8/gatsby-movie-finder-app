bfa56db393184b94fcbf6a455699e4a50f18bc01
shaonkabir8 <shaonkabir98@gmail.com>
2020-08-31 22:58:00 +0600

    fix(build): remove prettier config and build projects

---
+++ build/precache-manifest.2714eddbe1d580beb229f54bdd3ba24e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "39d99b2d73813661ceca",
+    "url": "/static/css/main.8328e725.chunk.css"
+  },
+  {
+    "revision": "39d99b2d73813661ceca",
+    "url": "/static/js/main.39d99b2d.chunk.js"
+  },
+  {
+    "revision": "75bfe83e46086925f3f6",
+    "url": "/static/css/1.3f9a5505.chunk.css"
+  },
+  {
+    "revision": "75bfe83e46086925f3f6",
+    "url": "/static/js/1.75bfe83e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "015abc8ec18339e85bbdada2360a5539",
+    "url": "/static/media/placeholder-img.015abc8e.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "9f0ce580873409aca8cb90e13f3ddb93",
+    "url": "/index.html"
+  }
+];
